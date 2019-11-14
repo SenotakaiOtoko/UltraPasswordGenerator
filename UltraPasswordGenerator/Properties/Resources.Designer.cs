6c43f4251f7b9b49ab9515b87580b9bf5dbffda9
HOSTCO\rybakov <V.Rybakov@hostco.ru>
2019-11-14 20:50:45 +0500

    Добавить словарь слов

--- UltraPasswordGenerator/Properties/Resources.Designer.cs
+++ UltraPasswordGenerator/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace UltraPasswordGenerator.Properties
-{
-
-
+namespace UltraPasswordGenerator.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,108 @@ namespace UltraPasswordGenerator.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("UltraPasswordGenerator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to a
+        ///abandon
+        ///abandoned
+        ///abate
+        ///abbey
+        ///abbreviation
+        ///ABC
+        ///abide
+        ///ability
+        ///able
+        ///aboard
+        ///abode
+        ///abominable
+        ///about
+        ///above
+        ///abroad
+        ///abrupt
+        ///abruptly
+        ///absence
+        ///absent
+        ///absolute
+        ///absolutely
+        ///absorb
+        ///absorbed
+        ///absorption
+        ///abstract
+        ///absurd
+        ///absurdity
+        ///abundance
+        ///abundant
+        ///acacia
+        ///accent
+        ///accept
+        ///accepted
+        ///access
+        ///accessible
+        ///accident
+        ///accidental
+        ///accidentally
+        ///acclaim
+        ///accommodate
+        ///accommodation
+        ///accompany
+        ///accompanying
+        ///accomplice
+        ///accomplish
+        ///accomplished
+        ///accord
+        ///accordance
+        ///according
+        ///accordingly
+        ///account
+        ///accounts
+        ///accuracy
+        ///accurate
+        ///accusation
+        ///accuse
+        ///accused
+        ///accu [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string dictionary {
+            get {
+                return ResourceManager.GetString("dictionary", resourceCulture);
             }
         }
     }
